559cae44346c6c8712d979b88bccebc1db10257a
zoeznm <bomikim046@gmail.com>
2025-04-30 16:41:31 +0900

    MainPage 구현

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,9 @@
+import styles from './Main.module.scss';
+
+export default function MainPage() {
+  return (
+    <main className={styles.wrapper}>
+      <h2 className={styles.headline}>메인 페이지</h2>
+    </main>
+  );
+}
