376f1e15fc0d279862166d87be8a7d4c64c090d8
zoeznm <bomikim046@gmail.com>
2025-05-01 23:48:03 +0900

    메인페이지 스크롤 height 늘리기

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,25 @@ import styles from './Main.module.scss';
 export default function MainPage() {
   return (
     <main className={styles.wrapper}>
-      <h2 className={styles.headline}>메인 페이지</h2>
+      {/* 섹션 1 */}
+      <section className={styles.section}>
+        <h2 className={styles.headline}>SECTION 01</h2>
+      </section>
+
+      {/* 섹션 2 */}
+      <section className={styles.section}>
+        <h2 className={styles.headline}>SECTION 02</h2>
+      </section>
+
+      {/* 섹션 3 */}
+      <section className={styles.section}>
+        <h2 className={styles.headline}>SECTION 03</h2>
+      </section>
+
+      {/* 필요하면 더 추가 */}
+      <section className={styles.section}>
+        <h2 className={styles.headline}>SECTION 04</h2>
+      </section>
     </main>
   );
 }
