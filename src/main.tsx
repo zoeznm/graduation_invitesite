bbbe6b1f04a281ac2759bb1a2e7476d5c0f20287
zoeznm <bomikim046@gmail.com>
2025-05-03 20:36:07 +0900

    main.tsx: App 컴포넌트를 LanguageProvider로 감싸도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';                            // .tsx 확장자는 빼고
+import { LanguageProvider } from './contexts/LanguageProvider';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+if (!container) throw new Error('root 엘리먼트를 찾을 수 없습니다.');
+
+createRoot(container).render(
+  <React.StrictMode>
+    <LanguageProvider>                            {/* 여기에 감싸기 */}
+      <App />
+    </LanguageProvider>
+  </React.StrictMode>
+);
