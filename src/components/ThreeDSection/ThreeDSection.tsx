edc28e34734d3f831b6e9388f6c3e064b8f927b9
zoeznm <bomikim046@gmail.com>
2025-05-03 22:31:21 +0900

    전체적인 css 수정

--- src/components/ThreeDSection/ThreeDSection.tsx
+++ src/components/ThreeDSection/ThreeDSection.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './ThreeDSection.module.scss';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import { translations } from '../../contexts/lang/translations';
 
-const ThreeDSection: React.FC = () => (
-  <section className={styles.wrapper}>
-    <div className={styles.content}>
-      {/* Three.js 또는 react-three-fiber를 사용해 3D 모델을 렌더하세요 */}
-      <div className={styles.placeholder}>3D Object Here</div>
-    </div>
-    <div className={styles.scrollMore}>SCROLL FOR MORE</div>
-  </section>
-);
+const ThreeDSection: React.FC = () => {
+  const { lang } = useContext(LanguageContext);
+  const { scrollDown } = translations[lang];
+
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.content}>
+        {/* Three.js 또는 react-three-fiber를 사용해 3D 모델을 렌더하세요 */}
+        <div className={styles.placeholder}>3D Object Here</div>
+      </div>
+      <div className={styles.scrollDown}>{scrollDown}</div>
+    </section>
+  );
+};
 
 export default ThreeDSection;
