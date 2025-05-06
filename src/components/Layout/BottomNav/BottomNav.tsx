b286b01cbd453f2a479b21b0d0abfce91a3a690f
zoeznm <bomikim046@gmail.com>
2025-05-07 03:07:06 +0900

    하단 스크롤 ui 변경 완료

--- src/components/Layout/BottomNav/BottomNav.tsx
+++ src/components/Layout/BottomNav/BottomNav.tsx
@@ -1,3 +1,4 @@
+// src/components/Layout/BottomNav/BottomNav.tsx
 import React from 'react';
 import styles from './BottomNav.module.scss';
 
@@ -10,14 +11,20 @@ const PANEL_COUNT = 9;
 
 const BottomNav: React.FC<Props> = ({ currentPanel, progress }) => (
   <div className={styles.navigation}>
-    <div className={styles.navText}>
-      {String(currentPanel + 1).padStart(2, '0')} / {String(PANEL_COUNT).padStart(2, '0')}
-    </div>
+    {/* 1. SCROLL 레이블 */}
+    <div className={styles.navLabel}>SCROLL</div>
+
+    {/* 2. 진행바 */}
     <div className={styles.navProgress}>
       <div
         className={styles.navProgressFill}
         style={{ transform: `scaleX(${progress})` }}
       />
+    </div>
+
+    {/* 3. 패널 숫자 */}
+    <div className={styles.navText}>
+      {String(currentPanel + 1).padStart(2, '0')} / {String(PANEL_COUNT).padStart(2, '0')}
     </div>
   </div>
 );
